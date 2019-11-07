e3ce9c8acd5bcd8ee9b35c73365c9fe318ade500
Philippe Chevreau <pcu@3ds.com>
2019-11-07 15:22:14 -0700

    Updates

---
+++ precache-manifest.b1cfd1b3a91c33fc1beb8c9919289b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cacd3dd56ee4f3354b706ac76be696",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e6fd75f80e5314c763d1",
+    "url": "/react-tutorial/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "51498f5f4c10b3265b7e",
+    "url": "/react-tutorial/static/js/2.53cddcab.chunk.js"
+  },
+  {
+    "revision": "e6fd75f80e5314c763d1",
+    "url": "/react-tutorial/static/js/main.1ae71d86.chunk.js"
+  },
+  {
+    "revision": "acd1605a6af8f99b39b9",
+    "url": "/react-tutorial/static/js/runtime-main.b18dd318.js"
+  }
+]);
